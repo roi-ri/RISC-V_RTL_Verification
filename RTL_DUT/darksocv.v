1d514432520bbe2802a0cc7a6525bf89bfe90f56
Luis Diego Ramírez Leitón <ramirezluisleiton@gmail.com>
2026-05-12 13:01:03 -0600

    fix: Corregir ruta de darksocv.mem en la linea 104.

--- RTL_DUT/darksocv.v
+++ RTL_DUT/darksocv.v
@@ -101,7 +101,7 @@ module darksocv
 	 `elsif MODEL_TECH
 		  $readmemh("../../../../src/darksocv.mem",MEM);
     `else
-        $readmemh("../src/darksocv.mem",MEM,0);
+        $readmemh("darksocv.mem",MEM,0);         // Modificado. Antes: $readmemh("../src/darksocv.mem",MEM,0);
     `endif
     end
 
