3395306a54c5cc8e7f9bec1494e61038f4f31f6f
Rodrigo Sanchez A <rodrigo.e.sanchez.araya@gmail.com>
2026-05-12 18:48:18 -0600

    Feature: Version completa de interface.sv con la integracion de las entradas y salidas del modulo de darksocv.v
    Signed-off-by: Rodrigo Sanchez A <rodrigo.e.sanchez.araya@gmail.com>

---
+++ LayeredVerication/interface.sv
@@ -0,0 +1,32 @@
+/*
+* =============================================================================
+*
+* - File        : interface.sv
+* - Autor       : Rodrigo Sanchez Araya (C37259)
+* - Curso       : Verificación Funcional del Diseño de Circuitos Integrados
+* - Fecha       : 5/12/2026
+* - Descripción :Interfaz que agrupa las señales externas del DUT darksocv
+*                 utilizadas por el ambiente de verificación: reloj, reset, UART,
+*                 LEDs y DEBUG. Sirve como punto de conexión entre el testbench
+*                 y los componentes de verificación. 
+*
+* =============================================================================
+*/
+interface ifc_ricsv (
+    input logic XCLK
+);
+
+    logic XRES;
+
+    logic UART_RXD;
+    logic UART_TXD;
+
+    logic [3:0] LED;
+    logic [3:0] DEBUG;
+
+endinterface
+
+
+
+
+
