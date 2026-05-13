fb71146d67c7f23238eb85b569f69361b6748662
Luis Diego Ramírez Leitón <ramirezluisleiton@gmail.com>
2026-05-12 18:05:12 -0600

    feat: Crean casos para manejar los resultados teóricos y experimentales, según cada instrucción implementada.

--- LayeredVerication/monitor.sv
+++ LayeredVerication/monitor.sv
@@ -46,18 +46,104 @@ class monitor;
         forever begin
             @(posedge ifc_darksocv_obj.clk);     // Se espera que se cargue la instrucción/valor.
             @(negedge ifc_darksocv_obj.clk);     // Se espera al negedge después del posedge, para asegurar estabilidad.
-            $display("Monitor-checker: 
-            El resultado teorico es %d. 
-            El resultado experimental es %d",
-            scoreboard_obj.return_ref_value(), ifc_adder_obj.out);
             
-            // Se revisa si ambos resultados coinciden o no: 
-            if (scoreboard_obj.return_ref_value() != ifc_adder_obj.out) begin 
-                $display("Monitor-checker: Los resultados no coinciden (Fallo).");
-            end else begin
-                $display("Monitor-checker: Los resultados coinciden (Paso).");
+            // Se revisa que el scoreboard tenga datos para comparar: 
+            if (scoreboard_obj.res_mem.size() == 0) begin
+                $display("Monitor-checker: No hay datos por comparar (scoreboard vacío).");
+                continue; 
             end 
+            
+            // Si hay datos, se carga el resultado teórico más antiguo.
+            // El scoreboard utiliza el push_back, entonces aquí se carga usando pop_front.
+            reference = scoreboard_obj.res_mem.pop_front(); 
+
+            // Se inicializan las variables que se crearon: 
+            resul_teorico       = 32'd0;
+            resul_experimental  = 32'd';
+            comparar            = 1'b0;
+            instruccion         = "No está implementada"; 
+
+            // Casos para los diferentes tipos de instrucciones (aquí según sea, se carga el valor de interés): 
+            // Faltan de implementar varios tipos aún.
+            case(reference.instr_type)
+
+                R_TYPE: begin
+                    // Si es un tipo R lo que cambia es el registro con el resultado:
+                    resul_teorico = reference.res_ref;
+                    resul_experimental = ifc_darksocv_obj.              //*********** Falta ver el dato de interés.
+                    comparar = 1'b1;
+                    instruccion = "R_TYPE"
+                end 
+
+                I_TYPE_ARITHMETIC: begin
+                    // Si es un tipo I aritmético lo que cambia es el registro con el resultado:
+                    resul_teorico = reference.res_ref;
+                    resul_experimental = ifc_darksocv_obj.              //*********** Falta ver el dato de interés.
+                    comparar = 1'b1;
+                    instruccion = "I_TYPE_ARITHMETIC"
+                end
+
+                I_TYPE_SHIFT: begin
+                    // Si es un tipo I de desplazamiento lo que cambia es el registro con el resultado:
+                    resul_teorico = reference.res_ref;
+                    resul_experimental = ifc_darksocv_obj.              //*********** Falta ver el dato de interés.
+                    comparar = 1'b1;
+                    instruccion = "I_TYPE_SHIFT"
+                end
+
+                B_TYPE: begin
+                    // Si es un tipo B lo que cambia es el PC:
+                    resul_teorico = reference.pc_ref_next;
+                    resul_experimental = ifc_darksocv_obj.              //*********** Falta ver el dato de interés.
+                    comparar = 1'b1;
+                    instruccion = "B_TYPE"
+                end
+
+                U_TYPE: begin // Para esta sólo se implementó lui (eso en el scoreboard).
+                    // Si es un tipo U lo que cambia es el registro con el resultado:
+                    resul_teorico = reference.res_ref;
+
+                    // Se realiza con un lui, por si se prueba otro de este tipo que no falle:
+                    if (reference.instr_name == "LUI") begin
+                        resul_experimental = ifc_darksocv_obj.              //*********** Falta ver el dato de interés.
+                        comparar = 1'b1;
+                        instruccion = "U_TYPE: LUI resultado escrito en registro."
+                    end else begin
+                        comparar = 1'b0;
+                        instruccion = "U_TYPE: Instrucción no implementada."
+                    end 
+                end
+
+                default: begin
+                    comparar = 1'b0;
+                    instruccion = "Tipo de instrucción no reconocida (no se ha implementado)."
+                end 
+
+            endcase
+
+            // Si comparar es 1, es decir, se encontró la instrucción:
+            if (comparar) begin
+                $display("Monitor-checker:");
+                $display("Instrucción: %s", reference.instr_name);
+                $display("Tipo: %0d", reference.instr_type);
+                $display("Resultado teórico: %h", resul_teorico);
+                $display("Resultado experimental: %h", resul_experimental);
+
+                 // Se revisa si ambos resultados coinciden o no: 
+                if (resul_teorico != resul_experimental) begin 
+                    $display("Monitor-checker: Los resultados no coinciden (Fallo).");
+                end else begin
+                    $display("Monitor-checker: Los resultados coinciden (Paso).");
+                end     
+            end else begin
+                $display("Monitor-checker:");
+                $display("Instrucción: %s", reference.instr_name);
+                $display("Tipo: %0d", reference.instr_type);
+                $display("Estado de la revisión: %s", instruccion);
+            end 
+                
         end
+
     endtask
 
 endclass
