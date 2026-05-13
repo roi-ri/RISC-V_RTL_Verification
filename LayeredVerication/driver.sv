8935baafb3e72fd4eb09a445c0cbaba913accc5a
Rodrigo Sanchez A <rodrigo.e.sanchez.araya@gmail.com>
2026-05-13 00:01:06 -0600

    Added: New instructions avilable for the driver
    Signed-off-by: Rodrigo Sanchez A <rodrigo.e.sanchez.araya@gmail.com>

--- LayeredVerication/driver.sv
+++ LayeredVerication/driver.sv
@@ -39,7 +39,11 @@ class riscv_driver;
     // los structs correspondientes y sus casos de construcción.
     instr_pkg::r_instr_t r_instr; 
     instr_pkg::i_arithmetic_instr_t i_arithmetic_instr;
-  
+    instr_pkg::i_shift_instr_t i_shift_instr; 
+    instr_pkg::i_load_instr_t i_load_instr;
+    instr_pkg::s_store_inst_t s_store_instr;
+    instr_pkg::i_jump_instr_t i_jump_instr;
+
 
     function new (virtual ifc_ricsv ifc_riscv_obj, scoreboard scoreboard_obj);
         this.ifc_riscv_obj = ifc_riscv_obj;
@@ -63,8 +67,7 @@ class riscv_driver;
         $display("Creando instruccion aleatoria");
         case (stimulus_obj.instr_type) 
             R_TYPE: begin
-                //Definicion de campos por defecto para
-                //cada instruccion 
+                //Definicion de campos por defecto para cada instruccion 
                 r_instr.opcode   = 7'b0110011; 
 
                 // Asignacion de las variables aleatorias
@@ -79,7 +82,7 @@ class riscv_driver;
                         //construccion final de la
                         //instruccion
                          instr_wrd = r_instr; 
-                        $display("Instruccion ADD creada: %d", instr_wrd);
+                        $display("Instruccion ADD creada: 0x%08h", instr_wrd);
                     end 
                     SUB: begin 
                         r_instr.funct7 = 7'b0100000;
@@ -87,7 +90,7 @@ class riscv_driver;
                         //construccion final de la
                         //instruccion
                          instr_wrd = r_instr; 
-                        $display("Instruccion SUB creada: %d", instr_wrd);
+                        $display("Instruccion SUB creada: 0x%08h", instr_wrd);
                     end 
                     SLL:begin 
                         r_instr.funct7 = 7'b0000000;
@@ -95,7 +98,7 @@ class riscv_driver;
                         //construccion final de la
                         //instruccion
                          instr_wrd = r_instr; 
-                        $display("Instruccion SLL creada: %d", instr_wrd);
+                        $display("Instruccion SLL creada: 0x%08h", instr_wrd);
                     end 
                     SLT: begin 
                         r_instr.funct7 = 7'b0000000;
@@ -103,15 +106,15 @@ class riscv_driver;
                         //construccion final de la
                         //instruccion
                          instr_wrd = r_instr; 
-                        $display("Instruccion SLT creada: %d", instr_wrd); 
+                        $display("Instruccion SLT creada: 0x%08h", instr_wrd); 
                     end 
                     SLTU: begin 
                         r_instr.funct7 = 7'b0000000;
                         r_instr.funct3 = 3'b011;
                         //construccion final de la
                         //instruccion
-                         instr_wrd = r_instr; 
-                        $display("Instruccion SLTU creada: %d", instr_wrd);
+                        instr_wrd = r_instr; 
+                        $display("Instruccion SLTU creada: 0x%08h", instr_wrd);
                     end 
                     XOR: begin
                         r_instr.funct7 = 7'b0000000;
@@ -119,7 +122,7 @@ class riscv_driver;
                         //construccion final de la
                         //instruccion
                          instr_wrd = r_instr; 
-                        $display("Instruccion XOR creada: %d", instr_wrd);
+                        $display("Instruccion XOR creada: 0x%08h", instr_wrd);
                     end 
                     SRL: begin 
                         r_instr.funct7 = 7'b0000000;
@@ -127,7 +130,7 @@ class riscv_driver;
                         //construccion final de la
                         //instruccion
                          instr_wrd = r_instr; 
-                        $display("Instruccion SRL creada: %d", instr_wrd);
+                        $display("Instruccion SRL creada: 0x%08h", instr_wrd);
                     end 
                     SRA: begin
                         r_instr.funct7 = 7'b0100000;
@@ -135,7 +138,7 @@ class riscv_driver;
                         //construccion final de la
                         //instruccion
                          instr_wrd = r_instr; 
-                        $display("Instruccion SRA creada: %d", instr_wrd);
+                        $display("Instruccion SRA creada: 0x%08h", instr_wrd);
                     end
                     OR: begin
                         r_instr.funct7 = 7'b0000000;
@@ -143,7 +146,7 @@ class riscv_driver;
                         //construccion final de la
                         //instruccionss
                          instr_wrd = r_instr; 
-                        $display("Instruccion OR creada: %d", instr_wrd);
+                        $display("Instruccion OR creada: 0x%08h", instr_wrd);
                     end 
                     AND: begin
                         r_instr.funct7 = 7'b0000000;
@@ -151,7 +154,7 @@ class riscv_driver;
                         //construccion final de la
                         //instruccion
                          instr_wrd = r_instr; 
-                        $display("Instruccion AND creada: %d", instr_wrd);
+                        $display("Instruccion AND creada: 0x%08h", instr_wrd);
                     end
                     default: begin instr_wrd = 32'h00000013; // NOP: addi, x0, x0, 0
                         $display("Error en creacion de la instruccion");
@@ -161,69 +164,190 @@ class riscv_driver;
  
                 
             I_TYPE_ARITHMETIC: begin 
-            //Definicion de campos por defecto para
-            //cada instruccion 
-            i_arithmetic_instr.opcode   = 7'b0010011; 
+                //Definicion de campos por defecto para
+                //cada instruccion 
+                i_arithmetic_instr.opcode   = 7'b0010011; 
 
-            // Asignacion de las variables aleatorias
-            // creadas en el stimulus.sv
-            i_arithmetic_instr.rd       = stimulus_obj.rd;
-            i_arithmetic_instr.rs1      = stimulus_obj.rs1;
-            i_arithmetic_instr.imm      = stimulus_obj.imm_i; 
+                // Asignacion de las variables aleatorias
+                // creadas en el stimulus.sv
+                i_arithmetic_instr.rd       = stimulus_obj.rd;
+                i_arithmetic_instr.rs1      = stimulus_obj.rs1;
+                i_arithmetic_instr.imm      = stimulus_obj.imm_i; 
 
             case (stimulus_obj.i_arith_instr) 
                 ADDI: begin 
                     i_arithmetic_instr.funct3 = 3'b000;
                     //construccion final de la
-                    //instruccion
+                     //instruccion
                      instr_wrd = i_arithmetic_instr; 
-                    $display("Instruccion ADDI creada: %d", instr_wrd);
+                    $display("Instruccion ADDI creada: 0x%08h", instr_wrd);
                 end 
                 SLTI: begin 
                     i_arithmetic_instr.funct3 = 3'b010;
                     //construccion final de la
                     //instruccion
                     instr_wrd = i_arithmetic_instr; 
-                    $display("Instruccion SLTI creada: %d", instr_wrd);
+                    $display("Instruccion SLTI creada: 0x%08h", instr_wrd);
                 end 
                 SLTIU:begin
                     i_arithmetic_instr.funct3 = 3'b011;
                     //construccion final de la
                     //instruccion
                     instr_wrd = i_arithmetic_instr; 
-                    $display("Instruccion ADDI creada: %d", instr_wrd);
+                    $display("Instruccion ADDI creada: 0x%08h", instr_wrd);
                 end
                 XORI: begin 
                     i_arithmetic_instr.funct3 = 3'b100;
                     //construccion final de la
                     //instruccion
                     instr_wrd = i_arithmetic_instr; 
-                    $display("Instruccion ADDI creada: %d", instr_wrd);
+                    $display("Instruccion ADDI creada: 0x%08h", instr_wrd);
                 end 
                 ORI: begin 
                     i_arithmetic_instr.funct3 = 3'b110;
                     //construccion final de la
                     //instruccion
                     instr_wrd = i_arithmetic_instr; 
-                    $display("Instruccion ADDI creada: %d", instr_wrd);
+                    $display("Instruccion ADDI creada: 0x%08h", instr_wrd);
                 end 
-                ANDI: begin
+    ANDI: begin
                     i_arithmetic_instr.funct3 = 3'b111;
                     //construccion final de la
-                    //instruccion
+    //instruccion
                     instr_wrd = i_arithmetic_instr; 
-                    $display("Instruccion ADDI creada: %d", instr_wrd);
+                    $display("Instruccion ADDI creada: 0x%08h", instr_wrd);
                 end 
                 default: begin 
                     instr_wrd = 32'h00000013; // NOP: addi, x0, x0, 0
                     $display("Error en creacion de la instruccion");
                 end
                 endcase 
+            end 
+            
+            I_TYPE_SHIFT: begin 
+                i_shift_instr.opcode = 7'b0010011;
+                i_shift_instr.shamt  = stimulus_obj.shamt; 
+                i_shift_instr.rs1    = stimulus_obj.rs1; 
+                i_shift_instr.rd     = stimulus_obj.rd; 
+
+                case(stimulus_obj.i_shift_instr)
+                    SLLI: begin
+                        i_shift_instr.funct7 = 7'b0000000;
+                        i_shift_instr.funct3 = 3'b001;
+                        instr_wrd = i_shift_instr; 
+                        $display("Instruccion SLLI creada: 0x%08h", instr_wrd);
+                    end 
+                    SRLI: begin 
+                        i_shift_instr.funct7 = 7'b0000000;
+                        i_shift_instr.funct3 = 3'b101;
+                        instr_wrd = i_shift_instr; 
+                        $display("Instruccion SRLI creada: 0x%08h", instr_wrd);
+                    end
+                    SRAI: begin
+                        i_shift_instr.funct7 = 7'b0100000;
+                        i_shift_instr.funct3 = 3'b101;
+                        instr_wrd = i_shift_instr; 
+                        $display("Instruccion SRAI creada: 0x%08h", instr_wrd);
+                    end 
+                default: begin 
+                    instr_wrd = 32'h00000013; // NOP: addi, x0, x0, 0
+                    $display("Error en creacion de la instruccion");
+                end 
+            endcase
             end
+
+            I_TYPE_LOAD: begin 
+                i_load_instr.opcode = 7'b0000011;
+                i_load_instr.rs1 = stimulus_obj.rs1; 
+                i_load_instr.rd = stimulus_obj.rd; 
+                i_load_instr.offset = stimulus_obj.imm_i; 
+
+                case(stimulus_obj.i_load_instr)
+                    LB: begin
+                        i_load_instr.funct3 = 3'b000;
+                        instr_wrd = i_load_instr;
+                         $display("Instruccion LB creada: 0x%08h", instr_wrd);
+                    end 
+                    LH: begin
+                        i_load_instr.funct3 = 3'b001;
+                        instr_wrd = i_load_instr;
+                         $display("Instruccion LH creada: 0x%08h", instr_wrd);
+                    end 
+                    LW: begin
+                        i_load_instr.funct3 = 3'b010;
+                        instr_wrd = i_load_instr;
+                         $display("Instruccion LW creada: 0x%08h", instr_wrd);
+                    end 
+                    LBU: begin
+                        i_load_instr.funct3 = 3'b100;
+                        instr_wrd = i_load_instr;
+                         $display("Instruccion LBU creada: 0x%08h", instr_wrd);
+                    end 
+                    LHU: begin
+                        i_load_instr.funct3 = 3'b101;
+                        instr_wrd = i_load_instr;
+                         $display("Instruccion LHU creada: 0x%08h", instr_wrd);
+                    end 
+                    default: begin 
+                        instr_wrd = 32'h00000013; // NOP: addi, x0, x0, 0
+                        $display("Error en creacion de la instruccion");
+                    end 
+                endcase 
+            end 
+
+            S_TYPE: begin 
+                s_store_instr.opcode = 7'b0100011;
+                s_store_instr.imm_11_5 = stimulus_obj.imm_s[11:5];
+                s_store_instr.imm_4_0 = stimulus_obj.imm_s[4:0];
+                s_store_instr.rs1 = stimulus_obj.rs1; 
+                s_store_instr.rs2 = stimulus_obj.rs2; 
+                case(stimulus_obj.s_instr)
+                    SB: begin 
+                        s_store_instr.funct3 = 3'b000;
+                        instr_wrd = i_load_instr;
+                         $display("Instruccion SB creada: 0x%08h", instr_wrd);
+                    end 
+                    SH: begin 
+                        s_store_instr.funct3 = 3'b001;
+                        instr_wrd = i_load_instr;
+                         $display("Instruccion SH creada: 0x%08h", instr_wrd);
+                    end
+                    SW: begin 
+                        s_store_instr.funct3 = 3'b010;
+                        instr_wrd = i_load_instr;
+                         $display("Instruccion SW creada: 0x%08h", instr_wrd);
+
+                    end 
+                    default: begin 
+                        instr_wrd = 32'h00000013; // NOP: addi, x0, x0, 0
+                        $display("Error en creacion de la instruccion");
+                    end 
+                endcase 
+            end 
+            I_TYPE_JUMP: begin 
+                i_jump_instr.opcode = 7'b1100111;
+                i_jump_instr.funct3 = 3'b000;
+                i_jump_instr.offset = stimulus_obj.imm_i;
+                i_jump_instr.rs1    = stimulus_obj.rs1;
+                i_jump_instr.rd     = stimulus_obj.rd;
+
+                case(stimulus_obj.i_jump_instr)
+                    JALR: begin 
+                        instr_wrd = i_jump_instr;
+                        $display("Instruccion JALR creada: 0x%08h", instr_wrd);
+                    end 
+                    default: begin
+                        instr_wrd = 32'h00000013;
+                        $display("Error en creacion de instruccion JALR");
+                    end
+            endcase 
+
+            end     
+
 
 
             // TODO: Implementar las familias restantes de instrucciones:
-            // I_TYPE_SHIFT, I_TYPE_LOAD, I_TYPE_MEMORY_SYSTEM -> (Ver si se separa esta en dos), I_TYPE_JUMP,
+            // I_TYPE_LOAD, I_TYPE_MEMORY_SYSTEM -> (Ver si se separa esta en dos), I_TYPE_JUMP,
             // S_TYPE, B_TYPE, U_TYPE y J_TYPE. Mientras no estén soportadas,
             // se escribe una instrucción NOP para evitar insertar valores
             // inválidos en la memoria del DUT -> (aunque esto se esta limitando
@@ -233,10 +357,7 @@ class riscv_driver;
                 $display("Tipo de instruccion no implementado, escribiendo NOP");
             end 
 
-            default: begin
-                instr_wrd = 32'h00000013;
-                $display("Tipo de instruccion no implementado, escribiendo NOP");
-            end 
+            
         endcase 
         $root.top.dut.MEM[addr] = instr_wrd;
     endtask
